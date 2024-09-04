f4bc3acdd05359c6c46f793daa10eec8fb8826b8
Jesper Raemaekers <jesper.raemaekers@wefabricate.com>
2024-09-04 13:12:10 +0200

    Working user id get

--- weheat/abstractions/user.py
+++ weheat/abstractions/user.py
@@ -4,13 +4,14 @@ from weheat.api.user_api import UserApi
 
 
 def get_user_id_from_token(api_url: str, access_token: str):
+    """ Get the user id from the current logged-in user. """
     try:
         config = Configuration(host=api_url, access_token=access_token)
 
         with ApiClient(configuration=config) as client:
             response = UserApi(
                 client
-            ).api_v1_users_me_get_with_http_info
+            ).api_v1_users_me_get_with_http_info()
             if response.status_code == 200:
                 return response.data.id
     except Exception as e:
