0179cbaacae30c1b6fe0feedd1b7e8f765170b58
Jesper Raemaekers <jesper.raemaekers@wefabricate.com>
2024-09-04 15:06:36 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "weheat"
-VERSION = "2024.09.04"
+VERSION = "2024.09.05"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
