4f74a6f95910bb9948338ccc8d1d5e2d5cd289df
Jesper Raemaekers <jesper.raemaekers@wefabricate.com>
2025-12-24 07:54:05 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "weheat"
-VERSION = "2025.12.24rc"
+VERSION = "2025.12.24"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
--- weheat/api_client.py
+++ weheat/api_client.py
@@ -86,7 +86,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/2025.12.24rc/python'
+        self.user_agent = 'OpenAPI-Generator/2025.12.24/python'
         self.client_side_validation = configuration.client_side_validation
 
     async def __aenter__(self):
